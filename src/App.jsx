958c6197def2d646e207349e20b980eb0f2a5324
maundu55 <marymaundu.mm@gmail.com>
2024-11-24 14:08:37 +0300

    create a Color Toggler using React Js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h4>toggler</h4>
+    </>
+  )
+}
+
+export default App
---
+++ src/ToggleColor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToggleColor = () => {
+  return (
+    <div>ToggleColor</div>
+  )
+}
+
+export default ToggleColor
